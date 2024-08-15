29c1509aaca4452b6026a6b7de91dcf5912c37ca
AdnaNation <azizulislamadnan@gmail.com>
2024-08-15 15:16:41 +0600

    create:phones api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    fetch("/phones.json")
+      .then((res) => res.json())
+      .then((data) => setPhones(data));
+  }, []);
   return (
     <div>
       <h1>This is Home</h1>
+      Length: {phones.length}
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {phones.map((phone) => (
+          <div key={phone.phone_name} className="card glass">
+            <figure>
+              <img src={phone.image} alt="car!" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{phone.phone_name}</h2>
+              <p>{phone.brand}</p>
+              <p>${phone.price}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Learn now!</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
