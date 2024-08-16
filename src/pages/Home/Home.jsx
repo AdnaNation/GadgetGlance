bcac050a67dae6a9d7da119f6fcc6eabee356d8c
AdnaNation <azizulislamadnan@gmail.com>
2024-08-16 17:53:02 +0600

    create: register page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,18 +9,19 @@ const Home = () => {
   }, []);
   return (
     <div>
-      <h1>This is Home</h1>
-      Length: {phones.length}
-      <section className="grid grid-cols-1 md:grid-cols-3 gap-5">
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {phones.map((phone) => (
-          <div key={phone.phone_name} className="card glass">
+          <div key={phone.phone_name} className="card glass lg:mx-20">
             <figure>
-              <img src={phone.image} alt="car!" />
+              <img src={phone.image} alt={phone.phone_name} />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{phone.phone_name}</h2>
-              <p>{phone.brand}</p>
-              <p>${phone.price}</p>
+              <h2 className="card-title mx-auto">{phone.phone_name}</h2>
+              <div className="flex text-xs md:text-xl">
+                <p>Brand: {phone.brand}</p>
+                <p>Price: ${phone.price}</p>
+                <p>Rating: 4.99</p>
+              </div>
               <div className="card-actions justify-end">
                 <button className="btn btn-primary">Learn now!</button>
               </div>
